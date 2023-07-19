b082ec7ad692348494605c251fb2315cb65ae7b2
sorucoder <sorucoder+github@proton.me>
2023-07-19 11:14:33 -0400

    Updated cron scripts

--- .cron/backup.sh
+++ .cron/backup.sh
@@ -8,10 +8,10 @@ function email_message() {
     printf "</pre>\n"
 }
 
-printf "Beginning backup at $(date -Ins)..." >> /var/log/cron/backup.log
+printf "Beginning backup at $(date -Is)...\n" >> /var/log/cron/backup.log
 notify-send --app-name=Backup --icon=preferences-system-backup --urgency=CRITICAL "Backup" "Beginning backup..."
-if $HOME/.dotfiles/script/backup.sh >> /var/log/cron/backup.log 2>&1; then
-    notify-send --app-name=Backup --icon=preferences-system-backup "Backup" "Daily backup succeeded."
+if $HOME/.dotfiles/script/backup.sh -o /var/log/cron/backup.log; then
+    notify-send --app-name=Backup --icon=preferences-system-backup --urgency=CRITICAL "Backup" "Daily backup succeeded."
 else
     notify-send --app-name=Backup --icon=preferences-system-backup --urgency=CRITICAL "Backup" "Daily backup failed."
     email_message | mailx -M "text/html" -s "Backup Failed" sorucoder@proton.me
--- .cron/update.sh
+++ .cron/update.sh
@@ -8,9 +8,10 @@ function email_message() {
     printf "</pre>\n"
 }
 
-printf "Beginning update on $(date -Ins)..." >> /var/log/cron/update.log
+printf "Beginning update on $(date -Ins)...\n" >> /var/log/cron/update.log
 notify-send --app-name="System Update" --icon=system-software-update --urgency=CRITICAL "System Update" "Beginning system update..."
-if ! paru --noconfirm >> /var/log/cron/update.log 2>&1; then
+if ! paru --noconfirm | sed -e 's/\x1b\[[0-9;]*m//g' >> /var/log/cron/update.log 2>&1; then
+    notify-send --app-name="System Update" --icon=system-software-update --urgency=CRITICAL "System Update" "Daily backup failed."
     email_message | mailx -M "text/html" -s "Update Failed" sorucoder@proton.me
 fi
 reboot
